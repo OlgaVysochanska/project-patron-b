89d325398333112ed7500e36b7628a8454d41c77
IvanPanin675 <jovanni.94.van@gmail.com>
2023-05-12 19:22:21 +0300

    avatarDone

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,32 @@
+const fs = require("fs/promises");
+const path = require("path");
+const User = require("../../models/user");
+const Jimp = require("jimp");
+const { HttpError } = require("../../Helpers");
+
+const avatarDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const avatarName = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarDir, avatarName);
+    console.log(resultUpload);
+  await fs.rename(tempUpload, resultUpload);
+  console.log(resultUpload);
+  Jimp.read(resultUpload)
+    .then((image) => {
+      image.resize(250, 250).write(resultUpload);
+    })
+    .catch(() => {
+      throw HttpError(400, "Use type image: jpeg, png, bmp, tiff, gif");
+    });
+  const avatarURL = path.join("avatars", avatarName);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({
+    avatarURL,
+  });
+};
+
+module.exports = updateAvatar;
--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ const userSchema = new Schema(
       type: Array,
       default: [],
     },
-    favorite: {
+    favoriteAbs: {
       type: Array,
       default: [],
     },
