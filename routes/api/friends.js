5b8f50c07434915069af584c1b2d62c6d65729df
IvanPanin675 <jovanni.94.van@gmail.com>
2023-05-16 10:31:06 +0300

    fix-friends

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ctrlWrapper = require("../../helpers/ctrlWrapper");
-const getFriends = require("../../controllers/friends");
+const { getFriends } = require("../../controllers");
 
 const router = express.Router();
 
