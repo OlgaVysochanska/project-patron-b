6ca0298abdf38c0a2f17e259770ddfcf09b3fa63
hlushak <75501308+MarynaHl@users.noreply.github.com>
2023-05-17 14:58:48 +0100

    Added pagination to getFriends controller

--- controllers/friends/getFriends.js
+++ controllers/friends/getFriends.js
@@ -1,9 +1,29 @@
 const Sponsor = require("../../models/friends");
 
 const getFriends = async (req, res) => {
-  const result = await Sponsor.find();
+  const { page, pageSize } = req.query;
+  const pageNumber = parseInt(page) || 1;
+  const limit = parseInt(pageSize) || 10;
 
-  res.status(200).json(result);
+  const skip = (pageNumber - 1) * limit;
+
+  try {
+    const totalCount = await Sponsor.countDocuments();
+
+    const sponsors = await Sponsor.find().skip(skip).limit(limit);
+
+    const totalPages = Math.ceil(totalCount / limit);
+
+    res.status(200).json({
+      totalItems: totalCount,
+      currentPage: pageNumber,
+      totalPages: totalPages,
+      pageSize: limit,
+      friends: sponsors,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Помилка сервера" });
+  }
 };
 
 module.exports = getFriends;
