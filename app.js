5fb3eb638c0851f0f66db5a762d2585a0b8883e9
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-18 14:35:04 +0300

    swagger start

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const logger = require("morgan");
 const cors = require("cors");
 const {
@@ -17,6 +19,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/auth", authRouter);
 app.use("/api/notices", noticesRouter);
