d1366114acd197a3dd8f06b182c8f2d3d3c6b24e
IvanPanin675 <jovanni.94.van@gmail.com>
2023-05-19 18:33:38 +0300

    search-and-category

--- controllers/notices/getAllNotice.js
+++ controllers/notices/getAllNotice.js
@@ -2,10 +2,33 @@ const { HttpError } = require("../../helpers");
 const { Notice } = require("../../models/notice");
 
 const getAllNotice = async (req, res) => {
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, search, category } = req.query;
   const skip = (page - 1) * limit;
-  const data = await Notice.find({ skip, limit });
-  res.status(200).json(data);
+  const data = category ? await Notice.find({ category }, null, { skip, limit }) : await Notice.find({ skip, limit });
+
+  if (search) {
+    const searchLow = search.toLowerCase();
+    const findSearch = [];
+    for (const dat of data) {
+      let datName = dat.name.toLowerCase();
+      let datLocat = dat.location.toLowerCase();
+      let datSex = dat.sex.toLowerCase();
+      let datBreed = dat.breed.toLowerCase();
+      if (
+        datName.includes(searchLow) ||
+        datLocat.includes(searchLow) ||
+        datSex.includes(searchLow) ||
+        datBreed.includes(searchLow) 
+      ) {
+        findSearch.push(dat._id);
+      } 
+    }
+  const datafind = await Notice.find({_id:findSearch}, null, { skip, limit });
+    res.status(200).json(datafind);
+  } else {
+    res.status(200).json(data);
+  }
+
 };
 
 module.exports = getAllNotice;
