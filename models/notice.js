521de85f1a422dc331a580db1e9a37444e6f6c29
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-19 21:57:34 +0300

    fix

--- models/notice.js
+++ models/notice.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const handleSchemaErrors = require("../middlewares/handleSchemaErrors");
 
-const petsCategory = ["sell", "my pet", "lost-found", "for-free"];
+const petsCategory = ["sell", "lost-found", "for-free"];
 
 const validData = /^\d{2}.\d{2}.\d{4}$/;
 
