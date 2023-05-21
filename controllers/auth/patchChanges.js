8518f543750d8df81f661804799d1c36ee0d4c4e
IvanPanin675 <jovanni.94.van@gmail.com>
2023-05-21 16:17:34 +0300

    fix

--- controllers/auth/patchChanges.js
+++ controllers/auth/patchChanges.js
@@ -1,11 +1,22 @@
 const { User } = require("../../models");
 
 const patchChanges = async (req, res) => {
-  const { _id } = req.user;
+  const { _id, favoriteAbs: oldFavorite } = req.user;
 
-  await User.findByIdAndUpdate(_id, req.body);
+  const { favoriteAbs } = req.body;
+  if (favoriteAbs) {
+    if (oldFavorite.includes(favoriteAbs)) {
+      oldFavorite.splice(oldFavorite.indexOf(favoriteAbs), 1)
+    } else {
+      oldFavorite.splice(1, 0, favoriteAbs);
+    }    
+    await User.findByIdAndUpdate(_id, { favoriteAbs: oldFavorite });
+    res.json(oldFavorite);
+  } else {
+    await User.findByIdAndUpdate(_id, req.body);
 
-  res.json(req.body);
+    res.json(req.body);
+  }
 };
 
 module.exports = patchChanges;
