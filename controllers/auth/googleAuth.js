876c6b45230f6b89bc2788089d4ed7d55ca164e4
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-23 01:41:54 +0300

    edit

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -1,10 +1,6 @@
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const { User } = require("../../models");
-const { HttpError } = require("../../helpers");
-
-const { SECRET_KEY } = process.env;
 
 const googleAuth = async (req, res) => {
   const { _id: id } = req.user;
@@ -15,9 +11,17 @@ const googleAuth = async (req, res) => {
     expiresIn: "23h",
   });
 
-  await User.findByIdAndUpdate(id, { token });
+  const user = await User.findByIdAndUpdate(id, { token });
 
-  res.redirect(`http://localhost:3000?token=${token}`);
+  res
+    .status(201)
+    .json({
+      user: {
+        email: user.email,
+        token,
+      },
+    })
+    .redirect(`http://localhost:3000?token=${token}`);
 };
 
 module.exports = googleAuth;
