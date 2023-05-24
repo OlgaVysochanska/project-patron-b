f4acd6142c852111c9d35d20339d33c0d10bba9a
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-05-25 00:56:42 +0200

    Update getCategory.js

--- controllers/notices/getCategory.js
+++ controllers/notices/getCategory.js
@@ -4,7 +4,7 @@ const { Notice } = require("../../models/notice");
 
 const getCategory = async (req, res) => {
   const { category } = req.query;
-  const { page = 1, limit = 2 } = req.query;
+  const { page = 1, limit = 12 } = req.query;
   const data = await Notice.find({ category })
     .limit(limit * 1)
     .skip((page - 1) * limit);
