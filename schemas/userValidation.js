1997d07e2ede847ace460c53be70b0c940e32599
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-05-25 10:23:19 +0200

    Update userValidation.js

--- schemas/userValidation.js
+++ schemas/userValidation.js
@@ -7,7 +7,18 @@ const validPassword =
   /^(?=(?:[^A-Z]*[A-Z]){1,}[^A-Z]*$)(?=(?:[^a-z]*[a-z]){1,}[^a-z]*$)(?=(?:\D*\d){1,}\D*$)[A-Za-z\d]+/;
 
 const userValidation = Joi.object({
-  password: Joi.string().required().pattern(validPassword).min(6).max(16),
+  password: Joi.string()
+    .required()
+    .pattern(validPassword)
+    .messages({
+      "string.pattern.base":
+        "The password must contain at least 1 uppercase letter, 1 lowercase letter and 1 number",
+      "string.min": "Must have at least 6 characters",
+      "string.max": "Must have at least 16 characters",
+    })
+    .min(6)
+    .max(16),
+
   email: Joi.string().email().required(),
 });
 
